43062694f578921f11a7783716f9f09d30a9baa8
Daan Sprenkels <dsprenkels@science.ru.nl>
2015-05-29 17:20:14 +0200

    Resolved issue #3 (added smart shortcut buttons)

--- js/common.js
+++ js/common.js
@@ -10,3 +10,32 @@ javascript:String.prototype.rot13 = function(s)
   }
   ).join('');
 };
+
+/* vcieAddToProducts function will add n products to each input field of a
+ * category scaled by the values in `scale`
+ * @param {string[]} input_ids - The IDs the input fields to be affected
+ * @param {Object} scales - A dictionary containing the scale values for each input field by id
+ * @param {number} [n=1] - The unscaled amount of products to be added
+ */
+function vcieAddToProducts(input_ids, scales, n) {
+  n = typeof n !== 'undefined' ? n : 1;
+
+  $.each(input_ids, function _vcieAddToProductsEach(index, input_id) {
+    var HIDDEN_VALUE_DATA_NAME = "vcieRealValue";
+    var field = $(document.getElementById(input_id));
+    var hidden_value = field.data(HIDDEN_VALUE_DATA_NAME);
+    hidden_value = typeof hidden_value !== 'undefined' ? hidden_value : parseFloat(field.val());
+
+    if (isNaN(hidden_value)) {
+      hidden_value = 0.0;
+    }
+    else if (Math.round(hidden_value) !== parseInt(field.val())) {
+      hidden_value = parseFloat(field.val());
+    }
+
+    hidden_value += n * scales[input_id];
+    hidden_value = Math.max(hidden_value, 0);
+    field.data(HIDDEN_VALUE_DATA_NAME, hidden_value);
+    field.val(Math.round(hidden_value));
+  });
+}
